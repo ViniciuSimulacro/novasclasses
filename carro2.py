d3a46e1b286fc7ab5f007b34d07d068eb5fe9d9d
ViniciusSimulacro <delucca86@gmail.com>
2022-09-21 15:29:42 -0300

    Feitas alteracoes para protejer os atributos

--- carro2.py
+++ carro2.py
@@ -1,34 +1,46 @@
 class Carro:
     def __init__(self,cor, motor, gasolina):
-        self.cor = cor
-        self.motor = motor #opções: 'funcionando' ou 'quebrado'
-        self.gasolina = gasolina
+        self.__cor = cor
+        self.__motor = motor #opções: 'funcionando' ou 'quebrado'
+        self.__gasolina = gasolina
+
+    @property
+    def cor_do_carro(self):
+        a = self.__cor
+        return print(f' o carro é da cor {a}')
+
+    @cor_do_carro.setter
+    def mudando_a_cor(self, novacor = str):
+        self.__cor = novacor
 
     def muda_cor(self):
-        print(f'A cor do seu carro é {self.cor}')
+        print(f'A cor do seu carro é {self.__cor}')
         mudando_cor = input(str('Para qual cor vc gostaria de mundar: '))
-        self.cor = mudando_cor
-        print(f'Agora a cor de seu carro é {self.cor}')
+        self.__cor = mudando_cor
+        print(f'Agora a cor de seu carro é {self.__cor}')
 
     def conserto_de_motor(self):
-        print(f'O seu motor esta {self.motor}')
-        if self.motor == 'quebrado':
+        print(f'O seu motor esta {self.__motor}')
+        if self.__motor == 'quebrado':
             consertar = str(input('Gostaria de consertar seu motor (s/n): '))
             if consertar == 's':
-                self.motor = 'funcionando'
-                print(f'Você gastou R$ 3.000,00 para consertar o seu motor, agora ele esta {self.motor}')
+                self.__motor = 'funcionando'
+                print(f'Você gastou R$ 3.000,00 para consertar o seu motor, agora ele esta {self.__motor}')
             if consertar == 'n':
-                print('Então quero ver você mesmo consertar essa bosta...rs')
+                print('Então quero ver você mesmo consertar....rs')
 
-    def abastecer(self):
-        print(f'O seu carro tem {self.gasolina} litros!')
-        if self.gasolina <= 10:
+    def consulta_gasolina(self):
+        print(f'O seu carro tem {self.__gasolina} litros!')
+        if self.__gasolina <= 10:
             print('Você precisa abastercer!')
         else:
             print('Você ainda tem gasolina pra andar!')
 
-
-meucarango = Carro('azul','quebrado',10)
-#meucarango.muda_cor()
-#meucarango.conserto_de_motor()
-meucarango.abastecer()   
+    def muda_gasolina(self):
+        print(f'O seu carro tem {self.__gasolina} litros!')
+        colocar_gasolina = int(input('Voce gostaria de estar com quantos litros no tanque: '))
+        self.__gasolina = colocar_gasolina
+        if self.__gasolina < 10:
+            print('O carro vai parar...')
+        if self.__gasolina > 10:
+            print('O carro ainda deve andar bastante!)')
